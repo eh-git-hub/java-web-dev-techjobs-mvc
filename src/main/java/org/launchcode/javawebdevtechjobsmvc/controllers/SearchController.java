f5a42438798686d2a68e6abc06d883a5e535289e
Edgar Hernandez <EdgarHomeFolder@edgars-mbp.attlocal.net>
2021-03-01 21:21:54 -0600

    TODO3 and 4 complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -24,5 +24,17 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> searchResults;
 
+        if(searchType.toLowerCase().equals("all") || searchTerm.isBlank()){
+          searchResults = JobData.findAll();
+        }else{
+            searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("searchResults", searchResults);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
 }
